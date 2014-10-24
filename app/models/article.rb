f1bcfb38046284d5ae353a8fdd283340f300f8d6
gavindinubilo <gavin_dinubilo@mail.tmcc.edu>
2014-10-23 21:44:09 -0700

    Search is now working a lot better

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,12 @@ class Article < ActiveRecord::Base
             
   def self.search(search)
     if search
-      where(["title LIKE ? OR content[0] LIKE ?", "%#{search}%", "%#{search}%"])
+      # where(["title LIKE ? OR content[0] LIKE ?", "%#{search}%", "%#{search}%"])
+      where("lower(title) LIKE lower('% #{search} %') 
+             OR lower(content[1]) LIKE lower('% #{search} %') 
+             OR lower(content[2]) LIKE lower('% #{search} %') 
+             OR lower(content[3]) LIKE lower('% #{search} %')
+             OR lower(content[4]) LIKE lower('% #{search} %')")
     else
       none
     end
