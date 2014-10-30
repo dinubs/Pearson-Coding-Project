73bdc40d31dfd377c184c271ceec43ec3b48226a
gavindinubilo <gavin_dinubilo@mail.tmcc.edu>
2014-10-29 21:48:55 -0700

    PUPPIES

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,10 +15,18 @@
 //= require_tree .
 //= require bootstrap-datepicker
 
-$(document).on ('ready page:load',function(){
+$.fn.exists = function(){
+    return $( ':not(' + this.selector + ')' ).length < $( '*' ).length;
+}
+document.addEventListener('page:change', function() {
+	if($(".home").length) {
+		document.getElementsByClassName("home")[0].className += ' animated fadeIn';
+	}
+  document.getElementById('primary-content').className += ' animated fadeIn';
 	$('p').linkify();
-	if($("body").hasClass("relax")) {
+	if($(".relax").exists()) {
 		var rand = Math.floor(Math.random() * 3103);
+		console.log("YES");
 		$.ajax({url:"https://api.giphy.com/v1/gifs/search?q=puppies&api_key=dc6zaTOxFJmzC&limit=3203&offset=0",success:function(data){
 			var rand = Math.floor(Math.random() * 100);
 			console.log(data.data[rand].images.original.url);
@@ -35,16 +43,6 @@ $(document).on ('ready page:load',function(){
   			}});
   		})
 	}
-});
-
-$.fn.exists = function(){
-    return $( ':not(' + this.selector + ')' ).length < $( '*' ).length;
-}
-document.addEventListener('page:change', function() {
-	if($(".home").length) {
-		document.getElementsByClassName("home")[0].className += ' animated fadeIn';
-	}
-  document.getElementById('primary-content').className += ' animated fadeIn';
 });
 document.addEventListener('page:fetch', function() {
 	if($(".home").length) {	
